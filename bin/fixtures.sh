af502484e647ae5f48cf36bc0f22e8532c17b1d0
Robert Norris <rob@eatenbyagrue.org>
2014-06-23 18:30:50 +1000

    add calendar service

--- bin/fixtures.sh
+++ bin/fixtures.sh
@@ -15,6 +15,7 @@ curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=web-client -F
 curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=login-sessions -F order=20 -F name='Login & sessions' -F description='Login & sessions'
 curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=mail-delivery -F order=30 -F name='Mail delivery' -F description='Mail delivery and routing'
 curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=mail-access -F order=40 -F name='Mail access (IMAP/POP)' -F description='Mail access via the IMAP and POP3 protocols'
+curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=calendar -F order=45 -F name='Calendar (CalDAV)' -F description='Calendar access via the CalDAV protocol'
 curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=file-storage -F order=50 -F name='File storage (FTP/DAV)' -F description='File storage access via the FTP and WebDAV protocols'
 curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=chat -F order=60 -F name='Chat (XMPP)' -F description='XMPP (Jabber) chat service'
 curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=contacts -F order=70 -F name='Contacts (LDAP)' -F description='Contacts access via the LDAP protocol'
