095417ac13e5d330f9cfdc78294913289202c7ab
Robert Norris <rob@eatenbyagrue.org>
2015-12-23 23:53:51 +1100

    cache content-type as well

--- lib/TownCrier.pm
+++ lib/TownCrier.pm
@@ -39,12 +39,15 @@ my $cache = CHI->new(driver => 'FastMmap', cache_size => '10m');
 sub cached (&) {
     my ($s) = @_;
     return sub {
+        content_type 'text/html';
         my $expiry_key = "_expiry_".request->uri;
         my $expires_in = $cache->get($expiry_key) // 1;
         my $out = $cache->compute(request->uri, $expires_in, sub {
             $cache->set($expiry_key, min($expires_in * 1.6, 3600));
-            $s->();
+            [$s->(), content_type()];
         });
+        content_type $out->[1];
+        $out->[0];
     };
 }
 
