3b2338a87598ffeaa9c4b8cf9c05f43da5fa3b66
Ricardo Signes <rjbs@semiotic.systems>
2022-10-13 14:24:57 -0400

    update fixture descriptions

--- bin/fixtures.sh
+++ bin/fixtures.sh
@@ -11,11 +11,11 @@ curl -u $AUTH -i http://localhost:3000/admin/api/v1/statuses -F name=Warning -F 
 curl -u $AUTH -i http://localhost:3000/admin/api/v1/groups -F name=Primary -F description='Primary services'
 
 # And services
-curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=web-client -F order=10 -F name='Web client' -F description='Access to services via the web client'
+curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=web-client -F order=10 -F name='Web client and mobile app' -F description='Access to services via the web client and mobile app'
 curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=login-sessions -F order=20 -F name='Login & sessions' -F description='Login & sessions'
 curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=mail-delivery -F order=30 -F name='Mail delivery' -F description='Mail delivery and routing'
 curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=mail-access -F order=40 -F name='Mail access (IMAP/POP)' -F description='Mail access via the IMAP and POP3 protocols'
 curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=calendar -F order=45 -F name='Calendar (CalDAV)' -F description='Calendar access via the CalDAV protocol'
 curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=file-storage -F order=50 -F name='File storage (FTP/DAV)' -F description='File storage access via the FTP and WebDAV protocols'
-curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=contacts -F order=70 -F name='Contacts (LDAP)' -F description='Contacts access via the LDAP protocol'
+curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=contacts -F order=70 -F name='Contacts (CardDAV)' -F description='Contacts access via the CardDAV protocol'
 curl -u $AUTH -i http://localhost:3000/admin/api/v1/services -F id=support -F order=80 -F name='Support System' -F description='Fastmail Support System'
